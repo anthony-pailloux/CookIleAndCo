9b7bbd4c7751e63fea71a96efa4ebfe6a1ae2ce1
Anthony Pailloux <220325852+anthony-pailloux@users.noreply.github.com>
2026-07-03 14:13:16 +0200

    feat(server): migration et modèle

---
+++ server/models/RecipeStep.js
@@ -0,0 +1,38 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database.js";
+
+class RecipeStep extends Model {
+    static associate(models) {
+
+    }
+}
+
+RecipeStep.init(
+    {
+        recipeId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        stepNumber: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        sortOrder: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize: sequelize,
+        modelName: 'RecipeStep',
+        tableName: 'recipe_steps',
+        underscored: true,
+        timestamps: false,
+    }
+);
+
+export default RecipeStep;
