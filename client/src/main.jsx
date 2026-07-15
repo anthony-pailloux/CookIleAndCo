a81530d21446ac55146b58224333b52d9add92b4
Anthony Pailloux <220325852+anthony-pailloux@users.noreply.github.com>
2026-07-15 13:06:01 +0200

    css hero

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './style/themes.css';
-import './index.css';
 import App from './App.jsx';
 
 // Point d'entrée React — monte App dans le DOM avec BrowserRouter (react-router-dom).
