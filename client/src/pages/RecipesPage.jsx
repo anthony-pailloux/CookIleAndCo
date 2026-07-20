e955c2e93b34f07e1b715b74aadcb0af22d3dda9
Anthony Pailloux <220325852+anthony-pailloux@users.noreply.github.com>
2026-07-20 14:38:06 +0200

    affichage client des recettes sur page recettes

--- client/src/pages/RecipesPage.jsx
+++ client/src/pages/RecipesPage.jsx
@@ -1,29 +1,38 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import { getFromApi } from "../services/api.js"
+import { useState, useEffect } from "react";
+import { getFromApi } from "../services/api.js";
 
 function RecipePage() {
-    const [recipes, setRecipes] = useState([]);
-    
-    useEffect(() => {
-        async function loadRecipes() {
-            const response = await getFromApi("/api/recipes?page=1&limit=12");
-            console.log(response);
-            setRecipes(response.recipes)
-        }
-        loadRecipes()
-    }, []);
+  const [recipes, setRecipes] = useState([]);
 
-    return (
-        
-        
-        <div>
-            <h2>Catalogue des recettes</h2>
-            <p>"Il y actuellement {recipes.length} recette en bdd"</p>
-        </div>
-        
-        
-    )
+  useEffect(() => {
+    async function loadRecipes() {
+      const response = await getFromApi("/api/recipes?page=1&limit=12");
+      setRecipes(response.recipes);
+    }
+
+    loadRecipes();
+  }, []);
+
+  let emptyMessage;
+
+  if (recipes.length === 0) {
+    emptyMessage = <p>Aucune recette pour le moment.</p>;
+  } else {
+    emptyMessage = null;
+  }
+
+  return (
+    <div>
+      <h2>Catalogue des recettes</h2>
+      {emptyMessage}
+
+      <ul>
+        {recipes.map((recipe) => {
+          return <li key={recipe.id}>{recipe.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default RecipePage;
