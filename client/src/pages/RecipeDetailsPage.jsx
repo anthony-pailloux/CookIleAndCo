b380f9bdb2daa83df2d4b33faadcd32a120d9440
Anthony Pailloux <220325852+anthony-pailloux@users.noreply.github.com>
2026-07-24 15:40:30 +0200

    css page produit

--- client/src/pages/RecipeDetailsPage.jsx
+++ client/src/pages/RecipeDetailsPage.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { getFromApi } from "../services/api";
 import { useParams } from "react-router-dom";
-import RecipeCard from "../components/RecipeCard";
 import IngredientItem from "../components/IngredientItem";
-
+import placeholderPhoto from "../assets/No_Image_Available.jpg";
+import "./RecipeDetailsPage.css";
+import "../components/RecipeCard.css";
 
 function RecipeDetailsPage() {
   const [recipeDetails, setRecipesDetails] = useState();
@@ -19,18 +20,60 @@ function RecipeDetailsPage() {
   }, [id]);
 
   let emptyMessage;
+  let photoSource;
 
   if (!recipeDetails) {
     emptyMessage = <p>Chargement...</p>;
+  } else {
+    if (recipeDetails.photo) {
+      photoSource = recipeDetails.photo;
+    } else {
+      photoSource = placeholderPhoto;
+    }
   }
+
 
   return (
     <div>
       {emptyMessage}
       {recipeDetails && (
         <>
-          <h1>{recipeDetails.title}</h1>
-          
+
+          <section className="recipe-hero">
+            <div className="recipe-card-photo-zone recipe-photo-zone-detail">
+              <img
+                className="recipe-card-photo recipe-details-placeholder-photo"
+                src={photoSource}
+                alt="recette-préparer"
+                onError={(event) => {
+                  event.currentTarget.src = placeholderPhoto;
+                }}
+              />
+            </div>
+            <h1>{recipeDetails.title}</h1>
+          </section>
+
+          <section className="recipe-content">
+            <div className="recipe-hero-info">
+              <h2>Ingrédients</h2>
+              <ul className="">
+                {recipeDetails.ingredients.map((ingredient) => {
+                  return (
+                    <li key={ingredient.id} className="">
+                      <IngredientItem ingredient={ingredient} />
+                    </li>
+                  );
+                })}
+              </ul>
+              
+            </div>
+
+            <div className="recipe-preparation">
+              <h2 className="recipe-preparation-title">Préparation</h2>
+              <p>Temps de cuisson: {recipeDetails.cookingTime}mn</p>
+            </div>
+          </section>
+
         </>
       )}
     </div>
