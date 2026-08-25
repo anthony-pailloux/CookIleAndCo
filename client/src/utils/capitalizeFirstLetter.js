bd579257e361ef58a3b69c842dadc636e8ad1b35
Anthony Pailloux <220325852+anthony-pailloux@users.noreply.github.com>
2026-08-25 11:30:12 +0200

    correctif css et Capital premiere lettre formulaire correctif

--- client/src/utils/capitalizeFirstLetter.js
+++ client/src/utils/capitalizeFirstLetter.js
@@ -1,10 +1,4 @@
 // Première lettre en majuscule (saisie formulaire).
 export function capitalizeFirstLetter(text) {
-  if (text === "") {
-    return "";
-  }
-
-  const firstLetter = text.charAt(0).toUpperCase();
-  const rest = text.slice(1);
-  return firstLetter + rest;
+  return text.charAt(0).toUpperCase() + text.slice(1);
 }
