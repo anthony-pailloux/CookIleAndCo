0704130ce92cf0292b099aaedf8ef922dd39c6fb
Anthony Pailloux <220325852+anthony-pailloux@users.noreply.github.com>
2026-08-25 11:34:19 +0200

    correctif footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,8 +3,14 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="site-footer bg-green-textured">
-      <p>© 2026 Tetelle Cook'île & Co</p>
-      <span>By AnthonyDevBdx</span>
+      <p>© 2026 Cook'île & Co</p>
+      <a
+        href="https://anthonydev.fr/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        By AnthonyDev
+      </a>
     </footer>
   );
 }
